899a6efab05ac082fc4196a097b736817e1b49b4
Kyza <kyza@kyza.net>
2021-10-01 23:27:24 -0400

    1,000 suns but renderer

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
+// TODO: TYYYYYPPPPPPIIIINNNNNNGGGGGGSSSSSSSSS
+
 // @ts-ignore
-kernel.ipc.invoke("KERNEL_GET_RENDERER_PACKAGES").then(async (ogre) => {
-	for (const layer of ogre) {
-		for (const [id, pack] of Object.entries(layer)) {
-			const packModule = await import("import://" + pack);
-			const packClass = packModule.default ? packModule.default : packModule;
-			const packInstance = new packClass();
-			packInstance.start();
-		}
+const ogre = kernel.packages.getRendererPackages();
+
+const packageInstances = {};
+
+// TODO: Load efficiently with Promise.all.
+for (const layer of ogre) {
+	for (const [id, pack] of Object.entries(layer)) {
+		const packModule = await import("import://" + pack);
+		const packClass = packModule.default ? packModule.default : packModule;
+		const packInstance = new packClass();
+		packageInstances[id] = packInstance;
+		packInstance.start?.();
 	}
+}
+
+// @ts-ignore
+kernel.packages.onPackageStart((data) => {
+	console.log(data);
 });
+// @ts-ignore
+kernel.packages.onPackageStop((data) => {
+	console.log(data);
+});
+
+// @ts-ignore
+kernel.sendFinished();
+
+export {};

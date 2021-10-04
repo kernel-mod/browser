985760c5187f99ef3318124388f83601d1aab70d
Kyza <kyza@kyza.net>
2021-10-03 23:38:31 -0400

    Remove package loading for now

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 // TODO: TYYYYYPPPPPPIIIINNNNNNGGGGGGSSSSSSSSS
 
 // @ts-ignore
-const ogre = kernel.packages.getRendererPackages();
+// const ogre = kernel.packages.getRendererPackages();
 
 const packageInstances = {};
 
 // TODO: Load efficiently with Promise.all.
-for (const layer of ogre) {
-	for (const [id, pack] of Object.entries(layer)) {
-		const packModule = await import("import://" + pack);
-		const packClass = packModule.default ? packModule.default : packModule;
-		const packInstance = new packClass();
-		packageInstances[id] = packInstance;
-		packInstance.start?.();
-	}
-}
+// for (const layer of ogre) {
+// 	for (const [id, pack] of Object.entries(layer)) {
+// 		const packModule = await import("import://" + pack);
+// 		const packClass = packModule.default ? packModule.default : packModule;
+// 		const packInstance = new packClass();
+// 		packageInstances[id] = packInstance;
+// 		packInstance.start?.();
+// 	}
+// }
 
-// @ts-ignore
-kernel.packages.onPackageStart((data) => {
-	console.log(data);
-});
-// @ts-ignore
-kernel.packages.onPackageStop((data) => {
-	console.log(data);
-});
+// // @ts-ignore
+// kernel.packages.onPackageStart((data) => {
+// 	console.log(data);
+// });
+// // @ts-ignore
+// kernel.packages.onPackageStop((data) => {
+// 	console.log(data);
+// });
 
 // @ts-ignore
 kernel.sendFinished();
